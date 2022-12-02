1865462be7bc254484ca67fefd4756fc454383ad
Paul-Nicolas Madelaine <pnm@pnm.tf>
2022-12-02 08:00:00 +0100

    add secrets

--- actions.nix
+++ actions.nix
@@ -1,6 +1,8 @@
 { stdenv, writeShellScript }:
 
-let echo = writeShellScript "echo" "cat";
+let
+  echo = writeShellScript "echo" "cat";
+  secrets = ./secrets.age;
 in stdenv.mkDerivation {
   name = "actions";
   phases = [ "installPhase" ];
@@ -8,5 +10,6 @@ in stdenv.mkDerivation {
     mkdir -p $out
     ln -s ${echo} $out/begin
     ln -s ${echo} $out/end
+    ln -s ${secrets} $out/secrets
   '';
 }
